3684982985832381a3aa5a78f02c87b8855afb05
Zach H <zhuffman90@gmail.com>
2014-12-23 00:39:22 -0600

    Commit fighter
    Just backing up for now. Will provide more info later.

---
+++ fighter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env
+
+from res import constants
+
+fighters = constants.FIGHTER_NAME_TO_ID
+
+class Fighter(object):
+    def __init__(self, name="", fighter_id=-1, palette=0):
+        if name == "" and fighter_id == -1:
+            raise ValueError("Fighter must be instantiated with either name or id")
+        if name not in fighters.keys():
+            if name != "":
+                raise ValueError("No fighter with name: {0}".format(name))
+            name = self.get_fighter_name_for_id(fighter_id)
+        if fighter_id not in fighters.values():
+            if fighter_id != -1:
+                raise ValueError("No fighter with id: {0}".format(fighter_id))            
+            fighter_id = self.get_fighter_id_for_name(name)
+        # TODO: some kind of check for valid palette swap
+        self.name = name
+        self.id = fighter_id
+        self.palette = pallete
+
+    # Return name associated with fighter id
+    def get_fighter_name_for_id(self, fighter_id):
+        for name in fighters.keys():
+            if fighter_id == fighters[name]:
+                return name
+
+    # Return id associated with fighter name
+    def get_fighter_id_for_name(self, name):
+        return fighters[name]
