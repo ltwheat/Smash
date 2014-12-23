2722885633b59426b7b7202ae2f90b2db16c7f3d
Zach H <zhuffman90@gmail.com>
2014-12-23 00:44:31 -0600

    Commit smasher
    Just backing up for now--will provide more info later.

---
+++ smasher.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env
+
+from res import constants
+
+smashers = constants.SMASHER_NAME_TO_ID
+
+class Smasher(object):
+    def __init__(self, name="", smasher_id=-1):
+        if name == "" and smasher_id == -1:
+            raise ValueError("Smasher must be instantiated with either name or id")
+        if name not in smashers.keys():
+            if name != "":
+                raise ValueError("No smasher with name: {0}".format(name))
+            name = self.get_smasher_name_for_id(smasher_id)
+        if smasher_id not in smashers.values():
+            if smasher_id != -1:
+                raise ValueError("No smasher with id: {0}".format(smasher_id))            
+            smasher_id = self.get_smasher_id_for_name(name)
+        self.name = name
+        self.id = smasher_id
+
+    # Return name associated with smasher id
+    def get_smasher_name_for_id(self, smasher_id):
+        for name in smashers.keys():
+            if smasher_id == smashers[name]:
+                return name
+
+    # Return id associated with smasher name
+    def get_smasher_id_for_name(self, name):
+        return smashers[name]
