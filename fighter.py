0f8486fdd9f47686078699f5ccb85c9ce877eea7
unknown <zhuffman90@gmail.com>
2015-02-22 22:17:27 -0600

    Added automatic capitalization to fighter

--- fighter.py
+++ fighter.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env
 
+import string
+
 from res import constants
 
 fighters = constants.FIGHTER_NAME_TO_ID
 
 class Fighter(object):
     def __init__(self, name="", fighter_id=-1):
+        cap_name = string.capwords(name)
         # Instantiation conditions
         if name == "" and fighter_id == -1:
             raise ValueError("Fighter must be instantiated with either name " +
                              "or id")
-        if name not in fighters.keys():
+        if cap_name not in fighters.keys():
             if name != "":
                 raise ValueError("No fighter with name: {0}".format(name))
             name = self.get_fighter_name_for_id(fighter_id)
         if fighter_id not in fighters.values():
             if fighter_id != -1:
                 raise ValueError("No fighter with id: {0}".format(fighter_id))            
-            fighter_id = self.get_fighter_id_for_name(name)
+            fighter_id = self.get_fighter_id_for_name(cap_name)
 
         # Set attributes
-        self.name = name
+        self.name = cap_name
         self.fighter_id = fighter_id
 
     # Return name associated with fighter id
@@ -31,7 +34,7 @@ class Fighter(object):
 
     # Return id associated with fighter name
     def get_fighter_id_for_name(self, name):
-        return fighters[name]
+        return fighters[string.capwords(name)]
 
     # Nicer than Figher.__dict__
     def convert_to_dict(self):
