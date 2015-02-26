82cde27d66d50657f4a1133b8bc5b995cd998f7b
unknown <zhuffman90@gmail.com>
2015-02-26 00:11:40 -0600

    Intelligent Smasher instantiation. Still needs a bit of work with mii_name

--- smasher.py
+++ smasher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env
 
-from res import constants
+from common_mongo import smash_conn
+from Smash.res import constants
 
 # TODO: There needs to be a distinction b/w tag and Nintendo Network name,
 #       e.g. "Lt Wheat" v "Mr. Wheat", since the latter is what shows up on
@@ -14,23 +15,71 @@ smashers = constants.SMASHER_NAME_TO_ID
 
 class Smasher(object):
     def __init__(self, mii_name="", tag="", smasher_id=-1):
+        # TODO: If mii_name is passed in WITH existing tag or id, we should
+        #       update the smasher's mii name in the coll
+        store = False
+        # TODO: This is too long, make it a separate method
         # Instantiation checks
         if mii_name == "" and tag == "" and smasher_id == -1:
             raise ValueError("Smasher must be instantiated with Mii name, " +
                              "tag, or id")
-        if tag not in smashers.keys():
-            if tag != "":
-                raise ValueError("No smasher with tag: {0}".format(tag))
-            tag = self.get_smasher_tag_for_id(smasher_id)
-        if smasher_id not in smashers.values():
-            if smasher_id != -1:
-                raise ValueError("No smasher with id: {0}".format(smasher_id))            
-            smasher_id = self.get_smasher_id_for_tag(tag)
+        elif tag != "" or smasher_id != -1:
+            # A tag or id was passed in, so this Smasher is important enough to
+            # store
+            if tag == '':
+                smasher_ids = self.get_all_smasher_ids()
+                if smasher_id not in smasher_ids:
+                    raise ValueError("Can't instantiate Smasher with only id" +
+                                     " and no tag")
+                else:
+                    # Smasher with this id exists, use it
+                    # TODO: Make this a separate method
+                    smasher = smash_conn.get_smasher({'smasher_id':smasher_id})
+                    mii_name = smasher['mii_name']
+                    tag = smasher['tag']
+                    smasher_id = smasher['smasher_id']
+            elif smasher_id == -1:
+                smasher_tags = self.get_all_smasher_tags()
+                if tag in smasher_tags:
+                    # Smasher with this tag exists, use it
+                    smasher = smash_conn.get_smasher({'tag':tag})
+                    mii_name = smasher['mii_name']
+                    tag = smasher['tag']
+                    smasher_id = smasher['smasher_id']
+                else:
+                    smasher_id = max(self.get_all_smasher_ids()) + 1
+                    store = True
+            else:
+                smasher = smash_conn.get_smasher({'smasher_id':smasher_id})
+                if smasher != None:
+                    if smasher['tag'] != tag:
+                        raise ValueError("Found existing Smasher with id " +
+                                     "{0} but different ".format(smasher_id) +
+                                     "tags: {0} v {1}".format(tag,
+                                                              smasher['tag']))
+                    else:
+                        # Smasher with this tag and id exists, use it
+                        mii_name = smasher['mii_name']
+                        tag = smasher['tag']
+                        smasher_id = smasher['smasher_id']
+                else:
+                    # No matching IDs, so this Smasher probably doesn't exist,
+                    # but check for duplicate tags
+                    if tag not in self.get_all_smasher_tags():
+                        store = True
+                    else:
+                        raise ValueError("Smasher with tag {0} ".format(tag) +
+                                         "already exists in the collection")
 
         # Set attributes
         self.mii_name = mii_name
         self.tag = tag
         self.smasher_id = smasher_id
+
+        # Store the Smasher, if we've already determined it doesn't exist
+        # in the collection
+        if store:
+            smash_conn.store_smasher(self)
 
     # Return name associated with smasher id
     def get_smasher_tag_for_id(self, smasher_id):
@@ -47,3 +96,29 @@ class Smasher(object):
         smasher = {"mii_name":self.mii_name, "tag":self.tag,
                    "smasher_id":self.smasher_id}
         return smasher
+
+    ### Do the methods that interact with mongo belong elsewhere? ###
+
+    # Get the specified attribute for all Smashers
+    def get_all_smasher_attr(self, attr):
+        attrs = []
+        smashers = smash_conn.get_all_smashers()
+        for smasher in smashers:
+            smasher_attr = smasher[attr]
+            if smasher_attr in attrs:
+                raise Exception("Uh oh! Two Smashers have the same " +
+                                "{0}".format(attr))
+            attrs.append(smasher_attr)
+        return attrs
+
+    # Get all Smasher tags
+    def get_all_smasher_tags(self):
+        return self.get_all_smasher_attr('tag')
+
+    # Get all Smasher Mii Names
+    def get_all_smasher_mii_names(self):
+        return self.get_all_smasher_attr('mii_name')
+
+    # Get all Smasher ids
+    def get_all_smasher_ids(self):
+        return self.get_all_smasher_attr('smasher_id')
