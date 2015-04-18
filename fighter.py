88f0c7f9a58de1c239e529e659ffba05e7024cdd
Zach Huffman <zhuffman90@gmail.com>
2015-04-18 16:29:33 -0500

    Fixing some naming bugs in Stage and Fighter

--- fighter.py
+++ fighter.py
@@ -8,7 +8,7 @@ fighters = constants.FIGHTER_NAME_TO_ID
 
 class Fighter(object):
     def __init__(self, name="", fighter_id=-1):
-        cap_name = string.capwords(name)
+        cap_name = name.title()
         # Instantiation conditions
         if name == "" and fighter_id == -1:
             raise ValueError("Fighter must be instantiated with either name " +
@@ -34,7 +34,7 @@ class Fighter(object):
 
     # Return id associated with fighter name
     def get_fighter_id_for_name(self, name):
-        return fighters[string.capwords(name)]
+        return fighters[name.title()]
 
     # Nicer than Figher.__dict__
     def convert_to_dict(self):
--- stage.py
+++ stage.py
@@ -16,14 +16,14 @@ class Stage(object):
         # TODO: Figure out a solution for the .title() exceptions
         cap_name = name.title()
         if 'yoshi' in name.lower() or 'pac' in name.lower() or \
-           'luigi' in name.lower() or '75' in name:
+           'luigi' in name.lower() or '75' in name or ' of ' in name:
             cap_name = name
         # Type/value checks
         if name == "" and stage_id == -1:
             raise ValueError("Stage must be instantiated with either name or id")
         if cap_name not in stages.keys():
             if name != "":
-                raise ValueError("No stage with name: {0}".format(name))
+                raise ValueError("No stage with name: {0}".format(cap_name))
             name = self.get_stage_name_for_id(stage_id)
         if stage_id not in stages.values():
             if stage_id != -1:
